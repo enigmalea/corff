863d7991f2605ec80590beb546f020e0332129fb
Amber <enigmalea@gmail.com>
2022-06-14 20:41:04 -0400

    added default status.

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,7 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		client.user.setPresence({ activities: [{ name: 'Dragon Age: Dreadwolf', type: 'PLAYING' }], status: 'online' });
 	},
 };
