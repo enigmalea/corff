2ac0d320381268015f4f9fea12516ab12d7cb5f7
enigmalea <enigmalea@gmail.com>
2024-08-13 10:39:32 -0400

    fix ping command to use bot name.

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction) {
+    const botName = interaction.client.user.username;
+    await interaction.reply(`My name is ${botName}`);
+  },
 };
