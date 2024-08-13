9a1ad0ad5a2d27f53559ce062dc28f935f6d1f35
enigmalea <enigmalea@gmail.com>
2024-08-13 12:01:09 -0400

    add coinflip command.

---
+++ commands/coinflip.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("coinflip")
+    .setDescription("Toss a coin."),
+
+  async execute(interaction) {
+    const coin = ["Heads", "Tails"];
+    const coinFlip = Math.floor(Math.random() * coin.length).toString();
+
+    interaction.channel.send(`${coin[coinFlip]}`);
+  },
+};
